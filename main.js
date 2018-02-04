b61bc0d9a7f79307deec341edf470427cef73eb8
unknown <noemims@gmail.com>
2018-02-04 14:19:42 +0100

    adding templating with lodash

--- main.js
+++ main.js
@@ -22,24 +22,29 @@ var data3 = {
     'Radius':'500',
     'url':'https://openbus.emtmadrid.es:9443/emt-proxy-server/last/geo/GetStopsFromStop.php'
 };
+var datosmios = {
+  x:33,
+  y:44,
+};
 
 var sendData = data;
 
 var jqxhr = $.ajax({
         method: 'POST',
         url: sendData.url,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
         data: sendData,
     })
     .done(function(response, textStatus, jqXHR) {
-        console.log(response);
+        var templateBusResponse = _.template($('#apiResponseTmpl').html());
+         $('#apiResponse').append(templateBusResponse(response.stop));
+        //$('#apiResponse').append(templateBusResponse(datosmios));
         //alert( "success" );
-    })
-    .fail(function() {
-        //alert( "error" );
-    })
-    .always(function() {
-        //alert( "complete" );
     });
+    //.fail(function() {
+        //alert( "error" );
+    //})
+    //.always(function() {
+        //alert( "complete" );
+    //});
+
+
